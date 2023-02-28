a7b9af94eb5322972fd68d5317775b6bd5afca5b
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-02-28 06:08:39 +0530

    updated without code smell

--- src/main/java/org/example/JDBCPoll.java
+++ src/main/java/org/example/JDBCPoll.java
@@ -1,32 +1,11 @@
 package org.example;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.logging.Logger;
-class DataBaseConnection{
-    static DataBaseConnection dbc=null;
-    static Logger l=Logger.getLogger("com.api.jar");
-    private DataBaseConnection(){}
-    Connection conn;
-    public static DataBaseConnection getDataBaseConnection(){
-        if(dbc==null)
-            dbc=new DataBaseConnection();
-        return dbc;
-    }
-    protected void newConnection(String url,String user,String pass) throws SQLException{
-        this.conn= DriverManager.getConnection(url,user,pass);
-        l.info("New Connection Connected Successfully");
-    }
-    protected void closeConnection() throws SQLException{
-        this.conn.close();
-        l.info("All Connection Closed Successfully");
-    }
-}
+
 public class JDBCPoll {
     public static void main(String[] args) throws SQLException {
-        Logger l=Logger.getLogger("com.api.jar");
+        Logger l=Logger.getLogger(JDBCPoll.class.getName());
         Scanner sc=new Scanner(System.in);
 
             l.info("Welcome to jdbc Connection Pool Demo Program");
